83f76b5a34ed77f6d1d3817a889cf06d6014b107
sabdulla <55761676+sabdulla@users.noreply.github.com>
2021-10-19 00:22:31 -0400

    Create share_of_int_collabs.sql

---
+++ sql/share_of_int_collabs.sql
@@ -0,0 +1,45 @@
+WITH papers as (
+SELECT 
+  distinct merged_id,
+  aff.country  
+FROM quad_ai.papers corpus  
+INNER JOIN quad_ai.paper_countries aff  USING (merged_id)
+WHERE 
+  corpus.year >= 2010 
+  and corpus.year <= 2020
+
+), 
+
+country_totals as (
+
+SELECT 
+  count(distinct merged_id) as total_papers, 
+  country
+FROM papers
+GROUP BY country), 
+
+collab_nums as (
+
+SELECT 
+  count(distinct merged_id) as num_collabs, 
+  papers.country as country_a
+FROM papers 
+INNER JOIN 
+  (SELECT 
+      merged_id, 
+      country as country_b 
+    FROM papers) using (merged_id)
+WHERE papers.country != country_b 
+GROUP BY papers.country
+)
+
+SELECT 
+  country, 
+  total_papers, 
+  num_collabs, 
+  100*(num_collabs / total_papers) as percent_papers_collabed
+FROM country_totals 
+INNER JOIN collab_nums on country = country_a
+ORDER BY total_papers desc
+
+
