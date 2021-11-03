e252099fb69d9e7f2be9d2ab66edec23f12d0d24
sabdulla <55761676+sabdulla@users.noreply.github.com>
2021-11-03 12:39:25 -0400

    Update pub_countries_for_granted_patents.sql

--- sql/pub_countries_for_granted_patents.sql
+++ sql/pub_countries_for_granted_patents.sql
@@ -1,25 +1,24 @@
 WITH patents as (
 SELECT  
       year, 
-      ai.publication_country, 
-      Simple_family_ID, 
-      ai.Priority_country, 
+      publication_country, 
+      family_ID, 
+      Priority_country, 
       filing_status
 FROM (
 SELECT *
   FROM
     (SELECT 
-        simple_family_id, 
+        family_id, 
         max(extract(year from publication_date)) as year
-    FROM quad_ai.ai_patent_features
-    GROUP BY simple_family_id) as patents_with_min_date 
-INNER JOIN quad_ai.ai_patent_features using (Simple_family_ID)
+    FROM quad_ai.patent_dates
+    GROUP BY family_id) as patents_with_min_date 
 ) ai
-JOIN quad_ai.merged_patent_metadata on family_id = cast(simple_family_id as string)
+JOIN quad_ai.merged_patent_metadata using(family_id)
 GROUP BY 
     year, 
     publication_country, 
-    Simple_family_ID, 
+    family_ID, 
     Priority_country,
     filing_status
 
@@ -29,13 +28,13 @@ GROUP BY
 SELECT 
     Priority_country, 
     Publication_country, 
-    count(distinct simple_family_id) as num_patents
+    count(distinct family_id) as num_patents
 FROM patents
 WHERE 
     filing_status = 'Grant' 
-    and year >= 2005 
+    and year >= 2010 
     and year <= 2020
-    and Priority_country = 'JP' -- change to "IN", "US", "AU" for India, USA, Australia, respectively 
+    and Priority_country = 'JP' --or "US" or "IN" or "AU"
 GROUP BY 
     Priority_country, 
     Publication_country
