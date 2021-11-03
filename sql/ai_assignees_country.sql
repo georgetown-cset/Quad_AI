8259e2dd62244904beb52c9cdd90faaf09f3e562
sabdulla <55761676+sabdulla@users.noreply.github.com>
2021-11-03 12:08:31 -0400

    Update ai_assignees_country.sql

--- sql/ai_assignees_country.sql
+++ sql/ai_assignees_country.sql
@@ -2,62 +2,62 @@
 
 WITH patents as (
   SELECT 
-  assignee_unique, 
+  assignee, 
   year, 
-  ai.publication_country, 
-  Simple_family_ID, 
-  ai.Priority_country, 
+  family_ID, 
+  Priority_country, 
   filing_status
   FROM (
   SELECT *
     FROM
-      (SELECT simple_family_id, max(extract(year from publication_date)) as year
-      FROM quad_ai.ai_patent_features
-      GROUP BY simple_family_id) as patents_with_min_date 
-  INNER JOIN `gcp-cset-projects.1790_patents.1790_ai_patents_all_quantitative_information` using (Simple_family_ID)
-  CROSS JOIN UNNEST(Assignees) as assignee_unique
+    (SELECT 
+     family_id, 
+     max(extract(year from publication_date)) as year
+    from  quad_ai.patent_dates
+    group by family_id) as patents_with_min_date 
+  INNER JOIN quad_ai.patent_assignees using (family_ID)
+  --CROSS JOIN UNNEST(Assignee) as assignee_unique
   ) ai
-  JOIN `gcp-cset-projects.unified_patents.merged_metadata` on family_id = cast(simple_family_id as string)
+  INNER JOIN `gcp-cset-projects.unified_patents.merged_metadata` using(family_ID)
   GROUP BY year, 
-  assignee_unique, 
-  publication_country, 
-  Simple_family_ID, 
-  Priority_country,
-  filing_status
+    assignee, 
+    family_ID, 
+    Priority_country,
+    filing_status
 
 )
 
 
 (SELECT 
     Priority_country, 
-    assignee_unique, 
-    count(distinct Simple_family_ID) as Num_patents, 
+    assignee, 
+    count(distinct family_ID) as Num_patents, 
     filing_status
 FROM patents 
 WHERE 
     (filing_status = 'Grant')
-    and year >= 2005 
+    and year >= 2010
     and year <= 2020
     and Priority_country = 'JP' --or "US" or "CN" or "IN" or "AU"
-GROUP BY Priority_country, assignee_unique, filing_status
+GROUP BY Priority_country, assignee, filing_status
 ORDER BY Num_patents desc
 LIMIT 25)
 ---------------------------JOINING WITH PATENT APPLICATIONS
 UNION ALL
 (SELECT 
     Priority_country, 
-    assignee_unique, 
-    count(distinct Simple_family_ID) as Num_patents, 
+    assignee, 
+    count(distinct family_ID) as Num_patents, 
     filing_status
 FROM patents 
 WHERE 
     (filing_status = 'Application')
-    and year >= 2005 
+    and year >= 2010 
     and year <= 2020
     and Priority_country = 'JP'
 GROUP BY 
     Priority_country, 
-    assignee_unique, 
+    assignee, 
     filing_status
 ORDER BY Num_patents desc
 LIMIT 25)
