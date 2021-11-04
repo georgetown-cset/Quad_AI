24e60b38a8b484107b23db4a12748505a8d5968e
sabdulla <55761676+sabdulla@users.noreply.github.com>
2021-11-04 11:49:21 -0400

    Create quad_total_ai_papers.sql

---
+++ sql/quad_total_ai_papers.sql
@@ -0,0 +1,12 @@
+SELECT  
+--getting total no of AI papers with at least 1 quad author and getting its global share
+  count(distinct merged_id) as num_papers, 
+  (count(distinct merged_id)/ (SELECT count(distinct merged_id) from quad_ai.ai_papers)) as prop_papers
+FROM quad_ai.ai_papers corpus 
+INNER JOIN quad_ai.paper_countries countries using (merged_id)
+WHERE  
+  corpus.year >= 2010 
+  and corpus.year <= 2020
+  and country in ("India", "United States", "Australia", "Japan")
+ORDER BY 
+  num_papers desc
