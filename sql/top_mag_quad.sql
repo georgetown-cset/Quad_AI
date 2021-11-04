c3f9005638136d438f87646d105f8d7a5ea30d5c
sabdulla <55761676+sabdulla@users.noreply.github.com>
2021-11-04 01:06:40 -0400

    Update and rename mag_all_countries.sql to top_mag_quad.sql

--- sql/top_mag_quad.sql
+++ sql/top_mag_quad.sql
@@ -1,8 +1,8 @@
-SELECT count(distinct merged_id) as num_papers, 
-  corpus.year, 
+SELECT count(distinct merged_id) as num_papers,  
   aff.country,  
   case when field_stats.name 
   in 
+  --from table where we order the top global mag ai fields
     (select * 
     from quad_ai.top_mag_ai)
    then field_stats.name else 
@@ -11,11 +11,12 @@ FROM quad_ai.top_fields, unnest (fields) as field_stats
 JOIN quad_ai.ai_papers corpus using (merged_id) 
 JOIN quad_ai.paper_countries aff  USING (merged_id)
 WHERE level = 1
-  and corpus.year >= 2010 and corpus.year <= 2020
+  and corpus.year >= 2010 
+  and corpus.year <= 2020
+  and country in ("India", "United States", "Australia", "Japan")
 GROUP BY 
-    year, 
     aff.country, 
     field_name 
 ORDER BY 
-    year, 
     num_papers desc
+
