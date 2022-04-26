5b3c6404cb94a11016d30a27352932464614b94b
sabdulla <55761676+sabdulla@users.noreply.github.com>
2022-04-26 13:30:25 -0400

    Update quad_companies_countries.sql

--- sql/quad_companies_countries.sql
+++ sql/quad_companies_countries.sql
@@ -2,13 +2,17 @@ select
     country_code,
     count(distinct uuid) as num_companies,
 from gcp_cset_crunchbase.organizations
+left join gcp_cset_crunchbase.organization_descriptions using (uuid)
 cross join unnest (category_list_split) as categories
 --ai fx for keywords 
 where (cat_data.isAI(short_description) is true 
+    or cat_data.isAI(description) is true 
     or categories like '%Artificial Intelligence%')
     --only companies thru 2021
-    and founded_on < "2022-01-01" #without this  61449 as of 2-10-2022
+    and founded_on < "2022-01-01
     --only active companies
     and closed_on is null
 group by country_code 
 order by num_companies desc
+
+
