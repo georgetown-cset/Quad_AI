ca38b4f3e6b3d5a47e1ac0323dc664b0219808f9
sabdulla <55761676+sabdulla@users.noreply.github.com>
2022-08-01 19:50:49 -0400

    Update ai_patents.sql

--- patent_sql/ai_patents.sql
+++ patent_sql/ai_patents.sql
@@ -6,11 +6,16 @@ select ai_patents.patent_id,
     year, 
     priority_country, 
     publication_country,
-    filing_status
+    case when (publication_country in ("JP") and legal_status in ("Active", "Expired - Fee Related", "Expired - Lifetime", "Expired")) 
+  then "Grant" 
+  when publication_country in ("CN") and legal_status in ('Active', "Expired - Fee Related", "Expired - Lifetime", 'Withdrawn-After Issue',
+    'Active-Reinstated','Revoked') 
+    ###note we are missing about 13,000 "Ceased" Chinese patents with this method, but that is almost exactly negated by 13,000 FP "Expired-Lifetime" Chinese patent grants, so we would expect this number to be still almost exactly correct for China. 
+  then "Grant" else filing_status end filing_status
 from unified_patents.ai_patents 
 inner join unified_patents.merged_metadata using(patent_id)
-inner join (select family_id, max(extract(year from publication_date)) as year
+inner join (select family_id, max(extract(year from first_priority_date)) as year
     from  unified_patents.patent_dates
     group by family_id) dates on ai_patents.family_id = dates.family_id
-inner join unified_patents.priority_country countries on ai_patents.family_id = countries.family_id
-where year >= 2010
+inner join unified_patents.priority_country countries on ai_patents.patent_id = countries.patent_id
+where year between 2010 and 2021
